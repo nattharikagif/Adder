09ac24622421cdbaea43e560ea616e7a76efbab7
nattharika <60050168@kmitl.ac.th>
2018-09-10 18:47:38 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Adder{
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int x = sc.nextInt();
+    int y = sc.nextInt(); 
+
+  System.out.println(x+y);
+    }
+}
